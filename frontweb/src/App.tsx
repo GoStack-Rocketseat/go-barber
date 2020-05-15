332712526f4b25bed2a160246fd31c8aba308ac3
Marcelo Ratton <rattones@marcelorattoncom>
2020-05-15 01:08:50 -0300

    Estrutura das páginas: Página de Login

---
+++ frontweb/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import SignIn from './pages/SignIn';
+import GlobalStyles from './styles/global';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <SignIn />
+    </>
+  );
+}
+
+export default App;
