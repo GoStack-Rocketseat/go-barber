67084614948e77eea8d18e87b0a1ebcc924cb729
Marcelo Ratton <rattones@marcelorattoncom>
2020-05-19 10:07:02 -0300

    Estrutura das páginas: Isolando componentes

--- frontweb/src/pages/SignIn/index.tsx
+++ frontweb/src/pages/SignIn/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi'
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi'
 
 import { Container, Content, Background } from './styles';
 import logoImg from '../../assets/logo.svg';
+
+import Button from '../../components/Button';
+import Input from '../../components/Input';
 
 const SignIn: React.FC = () => {
   return (
@@ -13,10 +16,10 @@ const SignIn: React.FC = () => {
         <form>
           <h1>Faça seu logon</h1>
 
-          <input placeholder="E-mail" />
-          <input placeholder="senha" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input name="password" type="password" icon={FiLock} placeholder="senha" />
 
-          <button type="submit">Entrar</button>
+          <Button type="submit">Entrar</Button>
 
           <a href="forgot">Esqueci minha senha</a>
         </form>
