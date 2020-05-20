f21ba875874e19825a8534531212169e0ef66d84
Marcelo Ratton <rattones@marcelorattoncom>
2020-05-20 11:06:46 -0300

    Estrutura da página: utilizando o Unform

--- frontweb/src/pages/SignUp/index.tsx
+++ frontweb/src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi'
+import { Form } from '@unform/web';
 
 import { Container, Content, Background } from './styles';
 import logoImg from '../../assets/logo.svg';
@@ -8,6 +9,10 @@ import Button from '../../components/Button';
 import Input from '../../components/Input';
 
 const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+
   return (
     <Container>
       <Background />
@@ -15,7 +20,7 @@ const SignUp: React.FC = () => {
       <Content>
         <img src={logoImg} alt="Go Barber" />
 
-        <form>
+        <Form onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
 
           <Input name="name" icon={FiUser} placeholder="Nome" />
@@ -23,7 +28,7 @@ const SignUp: React.FC = () => {
           <Input name="password" type="password" icon={FiLock} placeholder="senha" />
 
           <Button type="submit">Entrar</Button>
-        </form>
+        </Form>
         <a href="login">
           <FiArrowLeft />
           Voltar para logon
